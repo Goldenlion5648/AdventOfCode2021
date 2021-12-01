367b7349b097445eca3d2d1bbd39f2afa2364b99
Goldenlion5648 <coboudinot@gmail.com>
2021-12-01 00:21:12 -0500

    day 1 done

---
+++ 1.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('C:/Users/cobou/Documents/Python/adventOfCodeMisc/library')
+from AoCLibrary import *
+
+a = AdventInput("input1.txt")
+inp = a.data
+
+t = 0
+x = nums(inp)
+for i in range(len(x) - 1):
+    if x[i+1] > x[i]:
+        t += 1
+ans(t, False)
+
+z = 0
+prev = 0
+for i in range(1, len(x)):
+    if sum(x[i:i+3]) > prev:
+        z += 1
+    prev = sum(x[i:i+3])
+ans(z)
+
+
